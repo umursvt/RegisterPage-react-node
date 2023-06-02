73374bf04f6eae1804ad2da67a64bf34b77980fb
umursvt <umursavut@gmail.com>
2023-06-02 16:09:26 +0300

    RegisterPage React+Node

---
+++ src/Form.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000', {
+        username,
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+export default Form;
